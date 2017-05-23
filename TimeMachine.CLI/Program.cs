4a6c7f9a06f837fa2d88044917cb7dab2e62c978
idanizi <idanizi@gmail.com>
2017-05-23 15:28:20 +0300

    first skel

---
+++ TimeMachine.CLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeMachine.CLI
+{
+    class Program
+    {
+        /// <summary>
+        /// Application Start
+        /// </summary>
+        /// <remarks>
+        /// Author: Idan Izicovich
+        /// Creation Date: 2017-05-23
+        /// </remarks>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Time Machine!");
+            Console.WriteLine("App Designed & Developed by Idan Izicovich, 2017 - all rights reserved (C)");
+
+            // TODO - application logic here...
+            /**
+             * 1. Menu (delegates)
+             * 2. Events
+             * 3. calling Domain
+             * 3. the Domain Calls the service layer
+             * 
+             * Good Luck for me!
+             */
+
+
+            /* EXIT */
+            Console.ReadLine(); // Note: Stand by before exit
+            Console.WriteLine("Closing Application...");
+        }
+    }
+}
